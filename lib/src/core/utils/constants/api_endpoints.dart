54fbc46dfc8885226b65459702c618b0307e0a21
VikasAgrawal-04 <vikash.agarawal2012@gmail.com>
2023-11-15 15:35:41 +0530

    15-nov

--- lib/src/core/utils/constants/api_endpoints.dart
+++ lib/src/core/utils/constants/api_endpoints.dart
@@ -1,4 +1,8 @@
 class EndPoints {
   static const String register = '/admin-register';
   static const String login = '/admin-login';
+
+  //Dashboard
+  static const String newCaretakers = '/new-caretakers';
+  static const String allAppointments = '/all-todays-appointments';
 }
